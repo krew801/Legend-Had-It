844522830133fc1cce526499f0f5120294ffd3de
Alex Smith <alsmith@instructure.com>
2019-12-06 20:47:48 -0700

    Added route pages, starting to add styling. Having issues running the react server.

---
+++ legend-has-it/public/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Search from './Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <About />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ legend-has-it/public/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+       <nav>
+           <h3>Logo</h3>
+           <ul>
+               <li>About</li>
+               <li>Search</li>
+           </ul>
+       </nav>
+    </div>
+  );
+}
+
+export default Nav;
