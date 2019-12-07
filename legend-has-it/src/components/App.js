ae3ab9fa697df6ebc5a40f6f3eb6a9ca309ad154
Alex Smith <alsmith@instructure.com>
2019-12-06 22:13:55 -0700

    Got the react running in browser and have react routes working correctly so far

--- legend-has-it/src/components/App.js
+++ legend-has-it/src/components/App.js
@@ -10,11 +10,20 @@ function App() {
     <Router>
     <div className="App">
       <Nav />
-      <Route path="/about" component ={About}/>
-      <Route path="/search" component ={Search}/>
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/search" component={Search}/>
+      </Switch>
     </div>
     </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
--- legend-has-it/src/components/Nav.js
+++ legend-has-it/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Nav() {
   return (
@@ -7,8 +8,12 @@ function Nav() {
        <nav>
            <h3>Logo</h3>
            <ul className="nav-links">
+             <Link to="/about">
                <li>About</li>
+               </Link>
+               <Link to="/search">
                <li>Search</li>
+               </Link>
            </ul>
        </nav>
     </div>
