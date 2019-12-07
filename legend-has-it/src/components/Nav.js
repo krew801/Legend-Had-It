742cd421a6e7fbad2687e0d8bee2c3004b94b6de
Alex Smith <alsmith@instructure.com>
2019-12-07 00:02:50 -0700

    added search page. Working on displaying search results. Will be working on post page after

--- legend-has-it/src/components/Nav.js
+++ legend-has-it/src/components/Nav.js
@@ -3,15 +3,18 @@ import './App.css';
 import {Link} from 'react-router-dom';
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  }
   return (
     <div className="Nav">
        <nav>
            <h3>Logo</h3>
            <ul className="nav-links">
-             <Link to="/about">
+             <Link style={navStyle} to="/about">
                <li>About</li>
                </Link>
-               <Link to="/search">
+               <Link style={navStyle} to="/search">
                <li>Search</li>
                </Link>
            </ul>
--- legend-has-it/src/components/Search.js
+++ legend-has-it/src/components/Search.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Search() {
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://www.googleapis.com/youtube/v3/search/list')
+    const items = await data.json();
+  }
+
   return (
     <div className="Search">
-        <h1>Search Page</h1>
+      <h1>Search Page</h1>
+      {/* <!-- Load icon library --> */}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+      {/* <!-- The form --> */}
+      <form className="example" action="action_page.php">
+        <input type="text" placeholder="Find Your Path.." name="search" />
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
     </div>
   );
 }
